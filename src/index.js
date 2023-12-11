a4357108fc3453a79602ec8829405945d0e8d5d5
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-12-11 14:48:09 -0300

    Actualización de index js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>¡Hola, React!</h1>
+        <h1>Mi primera aplicación de ReactJS</h1>
       </div>
     )
   }
