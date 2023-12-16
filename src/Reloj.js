f96dbbcc5d006d8f6e7ece826c679ec1dc50d449
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-12-16 08:03:42 -0300

    Actualización

---
+++ src/Reloj.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+class Reloj extends Component {
+  
+  state = {
+    hora: {
+        horas: '00',
+        minutos: '00',
+        segundos: '00',
+      },
+
+   };
+
+  imprimirHora = () => {
+    //let hora = this.state.hora;
+    let fechaJS = new Date();
+    let horaAct = fechaJS.getHours()<10?
+'0'+fechaJS.getHours(): fechaJS.getHours();
+    let minsActs = fechaJS.getMinutes()<10?
+'0'+fechaJS.getMinutes(): fechaJS.getMinutes();
+    let segsActs = fechaJS.getSeconds()<10?
+'0'+fechaJS.getSeconds(): fechaJS.getSeconds();
+    this.setState({
+      hora: {
+          horas: horaAct,
+          minutos: minsActs,
+          segundos: segsActs,
+        },
+    });
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(this.imprimirHora, 1000);
+    this.setState({ intervalId }); // Save the interval ID for later cleanup
+  }
+
+  componentWillUnmount() {
+    if (this.state.intervalId) {
+      clearInterval(this.state.intervalId); // Clear the interval when component unmounts
+    }
+  }
+
+render() {
+
+    return (
+      <div className="d-flex justify-content-center align-items-center" style={{height: 90 + 'vh'}}>
+        <h1>{this.state.hora.horas}:{this.state.hora.minutos}:{this.state.hora.segundos}</h1>
+      </div>
+    )
+  }
+}
+
+export default Reloj
