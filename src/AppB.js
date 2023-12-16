365e33f5186fa8bb3c21ef8ba9afc2756b6e8249
Gonzalo Benites <benitesgonzalogaston@gmail.com>
2023-12-15 23:21:48 -0300

    Actualización

---
+++ src/AppB.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  horaProp = {hora:'0000'};
+  state = {
+    hora: {
+        horas: '00',
+        minutos: '00',
+        segundos: '00',
+      },
+     
+   };
+
+  imprimirHora = () => {
+    //let hora = this.state.hora;
+    let fechaJS = new Date();
+    let horaAct = fechaJS.getHours()<10? 
+'0'+fechaJS.getHours(): fechaJS.getHours();
+    let minsActs = fechaJS.getMinutes()<10? 
+'0'+fechaJS.getMinutes(): fechaJS.getMinutes();
+    let segsActs = fechaJS.getSeconds()<10? 
+'0'+fechaJS.getSeconds(): fechaJS.getSeconds();
+    this.setState({
+      hora: {
+          horas: horaAct,
+          minutos: minsActs,
+          segundos: segsActs,
+        },
+    });
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(this.imprimirHora, 1000);
+    this.setState({ intervalId }); // Save the interval ID for later cleanup
+  }
+
+  componentWillUnmount() {
+    if (this.state.intervalId) {
+      clearInterval(this.state.intervalId); // Clear the interval when component unmounts
+    }
+  }
+  
+  render() {
+    
+    return (
+      <h1 className="">{this.state.hora.horas}:{this.state.hora.minutos}:{this.state.hora.segundos}</h1>
+    )
+  }
+}
+
+export default App
